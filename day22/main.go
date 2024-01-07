73e58d0b6c35332193f78822e3f98f0ee2ed8142
Christian Raess <christian.raess@gmail.com>
2024-01-07 15:51:18 +0100

    Day 22 complete

--- day22/main.go
+++ day22/main.go
@@ -17,13 +17,18 @@
 // A line like 2,2,2~2,2,2 means that both ends of the brick are at the same coordinate
 // in other words, that the brick is a single cube.
 //
-// ground is at z=0
+// ground is at z=0, so lowest is 1
 //
-// Part 1: let brick fall down until they touch ground or a brick below them
+// Part 1:
 //
+//	let brick fall down until they touch ground or a brick below them
 //	identify which bricks can be disintegrated (do not support other bricks)
 //
 // Part 2:
+//
+//	sum up for all bricks that are structurally relevant, howm many
+//	other bricks falls down
+//
 // ---------------------------------------------------------------------------
 package main
 
@@ -31,44 +36,45 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	"time"
 
 	"github.com/cdr74/AdventOfCode2023/utils"
+	"github.com/pkg/profile"
+	"github.com/samber/lo"
 )
 
 // runTest defines whether test.data or actual.data should be used
-const runTest bool = true
+const runTest bool = false
 const TEST_FILE string = "test.data"
 const DATA_FILE string = "actual.data"
 
 // -------------------------- Common Code Section ----------------------------
 
 var brickMapByLowestZ map[int][]*Brick
-var bricks []*Brick
+var brickCount int = 0
 
-func removeBrickFromBrickMap(brick *Brick) {
-	slice := brickMapByLowestZ[brick.StartZ]
-	s := make([]*Brick, 0)
-	for idx, _ := range slice {
-		if *(slice[idx]) != *(brick) {
-			s = append(s, slice[idx])
+func deepCopyBrickMapByLowestZ(org map[int][]*Brick) map[int][]*Brick {
+	newMap := make(map[int][]*Brick)
+	for lowestZ, bricks := range org {
+		newBricks := make([]*Brick, len(bricks))
+		for i, brick := range bricks {
+			newBricks[i] = brick.Copy()
 		}
+		newMap[lowestZ] = newBricks
 	}
-	brickMapByLowestZ[brick.StartZ] = s
+	return newMap
 }
 
-func removeBrickFromList(brick *Brick) {
-	s := make([]*Brick, 0)
-	for idx, _ := range bricks {
-		if *(bricks[idx]) != *(brick) {
-			s = append(s, bricks[idx])
-		}
+func removeBrickFromBrickMap(ID int) {
+	for i := range brickMapByLowestZ {
+		brickMapByLowestZ[i] = lo.Filter(brickMapByLowestZ[i], func(b *Brick, i int) bool {
+			return b.ID != ID
+		})
 	}
-	bricks = s
 }
 
 // Brick represents a 3D brick
 type Brick struct {
+	ID                     int
 	StartX, StartY, StartZ int
 	EndX, EndY, EndZ       int
 }
@@ -83,10 +89,32 @@ func (b *Brick) Intersects(other *Brick) bool {
 		b.EndZ >= other.StartZ
 }
 
+func (b *Brick) intersectionFree() bool {
+	otherBricks := brickSliceFromMap()
+	for _, otherBrick := range otherBricks {
+		if b.ID != otherBrick.ID && b.Intersects(otherBrick) {
+			return false
+		}
+	}
+	return true
+}
+
+func (b *Brick) Copy() *Brick {
+	return &Brick{
+		ID:     b.ID,
+		StartX: b.StartX,
+		StartY: b.StartY,
+		StartZ: b.StartZ,
+		EndX:   b.EndX,
+		EndY:   b.EndY,
+		EndZ:   b.EndZ,
+	}
+}
+
 // CreateBricks creates a list of bricks
 func CreateBricks(input []string) {
 	brickMapByLowestZ = make(map[int][]*Brick)
-	bricks = make([](*Brick), 0)
+
 	for _, line := range input {
 		coordString := strings.Replace(line, "~", ",", -1)
 		points := strings.Split(coordString, ",")
@@ -94,27 +122,17 @@ func CreateBricks(input []string) {
 			panic("CreateBricks() - Invalid number of points in line")
 		}
 
-		startX := utils.StringToInt(points[0])
-		startY := utils.StringToInt(points[1])
-		startZ := utils.StringToInt(points[2])
-		endX := utils.StringToInt(points[3])
-		endY := utils.StringToInt(points[4])
-		endZ := utils.StringToInt(points[5])
-
-		if startX > endX || startY > endY || startZ > endZ {
-			panic("CreateBricks() - start coordinates are not smaller than end coordinates")
-		}
-
 		brick := Brick{
-			StartX: startX,
-			StartY: startY,
-			StartZ: startZ,
-			EndX:   endX,
-			EndY:   endY,
-			EndZ:   endZ,
+			ID:     brickCount,
+			StartX: utils.StringToInt(points[0]),
+			StartY: utils.StringToInt(points[1]),
+			StartZ: utils.StringToInt(points[2]),
+			EndX:   utils.StringToInt(points[3]),
+			EndY:   utils.StringToInt(points[4]),
+			EndZ:   utils.StringToInt(points[5]),
 		}
+		brickCount++
 
-		bricks = append(bricks, &brick)
 		if brickMapByLowestZ[brick.StartZ] == nil {
 			brickMapByLowestZ[brick.StartZ] = make([]*Brick, 0)
 		}
@@ -122,63 +140,77 @@ func CreateBricks(input []string) {
 	}
 }
 
+func brickSliceFromMap() []*Brick {
+	bricks := make([]*Brick, 0)
+	for i := range brickMapByLowestZ {
+		lo.ForEach(brickMapByLowestZ[i], func(b *Brick, _ int) {
+			bricks = append(bricks, b.Copy())
+		})
+	}
+	return bricks
+}
+
 // -------------------------- Puzzle part 1 ----------------------------------
 
-func moveBricksAtZDownByOne(z int) int {
+func moveBrickDownByOne(brick *Brick) bool {
+	if brick.StartZ <= 1 {
+		return false
+	}
+	// update brick coordinates
+	brick.EndZ--
+	brick.StartZ--
+
+	// check if brick intersects with any brick
+	movePossible := brick.intersectionFree()
+
+	if movePossible {
+		// move brick to new level in brickMapByLowestZ
+		if brickMapByLowestZ[brick.StartZ] == nil {
+			brickMapByLowestZ[brick.StartZ] = make([]*Brick, 0)
+		}
+		// add brick to new level
+		brickMapByLowestZ[brick.StartZ] = append(brickMapByLowestZ[brick.StartZ], brick)
+		// remove from old level in brickMapByLowestZ
+		oldZ := brick.StartZ + 1
+		brickMapByLowestZ[oldZ] = lo.Filter(brickMapByLowestZ[oldZ], func(b *Brick, i int) bool {
+			return *b != *(brick)
+		})
+	} else {
+		// undo move
+		brick.EndZ++
+		brick.StartZ++
+		return false
+	}
+
+	return true
+}
+
+func moveBricksAtZDownByOne(z int, fallenBricks []bool) int {
 	moveCnt := 0
 
 	for _, brick := range brickMapByLowestZ[z] {
-		if brick.StartZ <= 1 {
-			continue
-		}
-
-		// update brick coordinates
-		brick.EndZ--
-		brick.StartZ--
-		// check if brick intersects with any brick
-		movePossible := true
-		for _, otherBrick := range bricks {
-			if !(otherBrick == brick) && brick.Intersects(otherBrick) {
-				movePossible = false
-				break
+		moving := true
+		for moving {
+			if moveBrickDownByOne(brick) {
+				fallenBricks[brick.ID] = true
+				moveCnt++
+			} else {
+				moving = false
 			}
 		}
-		if movePossible {
-			// move to new level in brickMapByLowestZ
-			moveCnt++
-			if brickMapByLowestZ[brick.StartZ] == nil {
-				brickMapByLowestZ[brick.StartZ] = make([]*Brick, 0)
-			}
-			// add brick to new level
-			brickMapByLowestZ[brick.StartZ] = append(brickMapByLowestZ[brick.StartZ], brick)
-			// remove from old level in brickMapByLowestZ
-			removeBrickFromBrickMap(brick)
-		} else {
-			// undo move
-			brick.EndZ++
-			brick.StartZ++
-		}
-
 	}
+
 	return moveCnt
 }
 
 func letBricksFallDown() int {
-	// let bricks fall down until they touch ground or a brick below them
-	bricksMovedCount := 0
-	loopCnt := 0
-	for loopCnt > 0 {
-		loopCnt = 0
+	fallenBricks := make([]bool, brickCount)
 
-		// get all keys from brickMapByLowestZ
-		zKeys := make([]int, 0)
-		for k := range brickMapByLowestZ {
-			if brickMapByLowestZ[k] == nil || len(brickMapByLowestZ[k]) == 0 {
-				continue
-			}
-			zKeys = append(zKeys, k)
-		}
-		// sort keys
+	// let bricks fall down until they touch ground or a brick below them
+	done := false
+	for !done {
+		done = true
+		zKeys := lo.Keys(brickMapByLowestZ)
 		sort.Ints(zKeys)
 
 		for _, z := range zKeys {
@@ -186,88 +218,62 @@ func letBricksFallDown() int {
 			if z <= 1 {
 				continue
 			}
-			bricksMovedCount += moveBricksAtZDownByOne(z)
-			loopCnt++
+
+			moved := moveBricksAtZDownByOne(z, fallenBricks)
+			if moved > 0 {
+				done = false
+			}
 		}
 	}
-	return bricksMovedCount
-}
 
-func deepCopyOfBricks() []*Brick {
-	copy_bricks := make([](*Brick), 0)
-	for _, b := range bricks {
-		brick := Brick{b.StartX, b.StartY, b.StartZ, b.EndX, b.EndY, b.EndZ}
-		copy_bricks = append(copy_bricks, &brick)
-	}
-	return copy_bricks
-}
-
-func deepCopyOfMapByLowestZ() map[int][]*Brick {
-	copy_brickMapByLowestZ := make(map[int][]*Brick)
-	for k, slice := range brickMapByLowestZ {
-		copy_slice := make([]*Brick, 0)
-		for _, b := range slice {
-			brick := Brick{b.StartX, b.StartY, b.StartZ, b.EndX, b.EndY, b.EndZ}
-			copy_slice = append(copy_slice, &brick)
+	total := 0
+	for f := range fallenBricks {
+		if fallenBricks[f] {
+			total++
 		}
-		copy_brickMapByLowestZ[k] = copy_slice
 	}
-	return copy_brickMapByLowestZ
+	return total
 }
 
-func countStructuralRelevantBricks() int {
-	var copy_brickMapByLowestZ map[int][]*Brick
-	var copy_bricks []*Brick
+func countStructuralRelevantBricks() (int, int) {
+	totalMovedCount := 0
 	structuralRelevantBricks := 0
-	var loop_bricks = deepCopyOfBricks()
+
+	copy_brickMapByLowestZ := deepCopyBrickMapByLowestZ(brickMapByLowestZ)
+	loop_bricks := brickSliceFromMap()
 
 	for _, brick := range loop_bricks {
-		// save state of bricks and brickMapByLowestZ
-		copy_brickMapByLowestZ = deepCopyOfMapByLowestZ()
-		copy_bricks = deepCopyOfBricks()
-
 		// remove this brick, see if anything would move
-		removeBrickFromBrickMap(brick)
-		// remove from bricks
-		removeBrickFromList(brick)
+		removeBrickFromBrickMap(brick.ID)
 
 		bricksMovedCount := letBricksFallDown()
 		if bricksMovedCount > 0 {
+			totalMovedCount += bricksMovedCount
 			structuralRelevantBricks++
+			fmt.Printf("%v --v-- %d\n", brick, bricksMovedCount)
 		}
 
-		// restore state of global structures
-		brickMapByLowestZ = copy_brickMapByLowestZ
-		bricks = copy_bricks
+		// restore state of global brick data structure
+		brickMapByLowestZ = deepCopyBrickMapByLowestZ(copy_brickMapByLowestZ)
 	}
 
-	return structuralRelevantBricks
+	return len(loop_bricks) - structuralRelevantBricks, totalMovedCount
 }
 
-func SolvePart1() int {
+func SolvePart1_2() (int, int) {
 	letBricksFallDown()
-	for _, brick := range bricks {
-		fmt.Printf("%v\n", brick)
-	}
 	// find all bricks that are needed to support other bricks
-	return countStructuralRelevantBricks()
-}
-
-// -------------------------- Puzzle part 2 ----------------------------------
-
-func SolvePart2() int {
-	var result int = 0
-
-	return result
+	b, t := countStructuralRelevantBricks()
+	return b, t
 }
 
 // -------------------------- Main entry -------------------------------------
 
 func main() {
-	var input []string
+	// Enable CPU profiling
+	defer profile.Start(profile.ProfilePath(".")).Stop()
 
-	stopwatch := utils.NewStopwatch()
-	stopwatch.Start()
+	var input []string
 
 	if runTest {
 		input = utils.ReadDataFile(TEST_FILE)
@@ -276,14 +282,11 @@ func main() {
 	}
 
 	CreateBricks(input)
-	result1 := SolvePart1()
-	result2 := SolvePart2()
-	stopwatch.Stop()
+	result1, result2 := SolvePart1_2()
 
 	// ---------------------- Print results ----------------------------------
-	var elapsedTime time.Duration = stopwatch.GetElapsedTime()
+
 	fmt.Println("Running as test:\t", runTest)
 	fmt.Println("Result 1:\t\t", result1)
 	fmt.Println("Result 2:\t\t", result2)
-	fmt.Println("Elapsed time:\t\t", elapsedTime)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/cdr74/AdventOfCode2023
 
 go 1.18
+
+require (
+	github.com/pkg/profile v1.7.0
+	github.com/samber/lo v1.39.0
+)
+
+require (
+	github.com/felixge/fgprof v0.9.3 // indirect
+	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+)
